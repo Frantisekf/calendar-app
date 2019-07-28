6863b7ac5994af6f2f8d1d95dc1b15fbaa25c9d3
Frantisek Farkas <frantisekfarkas93@gmail.com>
2019-07-28 17:58:52 +0200

    init commit

---
+++ src/components/Calendar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+  name: 'Calendar',
+  data() {
+    return {
+      today: moment(),
+      dateContext: moment(),
+      days: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <calendar></calendar>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+
+import Calendar from '../components/Calendar.vue';
+
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    Calendar,
+
   },
 };
 </script>
